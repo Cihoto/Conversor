1fd2cfb11405c75a47848c406389b40a55caad07
Cihoto <67087066+Cihoto@users.noreply.github.com>
2020-06-18 00:14:16 -0400

    Add files via upload

---
+++ Conversor/Conversores.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversor
+{
+    public class Conversores
+    {
+
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public int numeroe1 { get; set; }
+        public int numeroe2 { get; set; }
+
+        public Conversores()
+        {
+        }
+
+        public Conversores(double numero1, double numero2, int numeroe1, int numeroe2)
+        {
+            Numero1 = numero1;
+            Numero2 = numero2;
+            this.numeroe1 = numeroe1;
+            this.numeroe2 = numeroe2;
+        }
+
+        public double MetrosaCentimetros(double num)
+        {
+            Numero1 = num;
+            double conversion = Numero1 / 100;
+            return conversion;
+
+        }
+
+        public int SegundosaMinutos(int segundos)
+        {
+            numeroe1 = segundos;
+            int conversion = numeroe1 / 60;
+            return conversion;
+
+        }
+
+        public double CelciusAFahrenheit(double Ce)
+        {
+            Numero1 = Ce;
+            double conversion = (Numero1 * 9 / 5) + 32;
+            return conversion;
+        }
+
+
+    }
+}
---
+++ Conversor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversores mac = new Conversores();
+            Console.WriteLine("Ingrese la cantidad de centimetros que desee covertir en metros");
+            double num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("La cantidad de metros es: " + mac.MetrosaCentimetros(num));
+            Console.WriteLine("Ingrese la cantidad de Segundos que desee covertir en Minutos");
+            int segundos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("La cantidad de minutos es  es: " + mac.SegundosaMinutos(segundos));
+            Console.WriteLine("Ingrese la cantidad de Grados Celcius que desee covertir en Grados Fahrenheit");
+            double grados = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("La cantidad de Grados Fahreheit son: " + mac.CelciusAFahrenheit(grados));
+        }
+    }
+}
